55620674fa5e5b9dbfd207936ed15d3a4b5d4a4d
Andri Möll <andri@dot.ee>
2015-08-14 22:22:19 +0300

    Fix rerunning tests by not reloading the Fetch polyfill.

--- test/fetch.js
+++ test/fetch.js
@@ -1,3 +1,7 @@
 global.self = {}
 try { require("whatwg-fetch"); module.exports = global.self.fetch }
 finally { delete global.self }
+
+// Cannot reload the polyfill as it mutates world state and doesn't return
+// exports.
+require("require-guard")()
